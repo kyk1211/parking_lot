dbd22c90e3fcce956b8d8944631e864f6fe7696e
곽윤구 <advsdvs@gmail.com>
2021-06-15 13:40:36 +0900

    'urlopen'

--- code.py
+++ code.py
@@ -1,14 +1,15 @@
 from urllib import request
+import urllib.parse
 servicekey = 'L%2Bx%2FuV2N4A5xeGN%2FJxtJ3idEtmvq7HMZeioTgu2kROPGZeUz0oSiz5zQiqUj94Oco4%2FcC%2BJNatV8hJf5mL%2BSuQ%3D%3D'
 url = 'http://api.data.go.kr/openapi/tn_pubr_prkplce_info_api'
 pagenum = '0'
 type = 'json'
 rows = '100'
 queryParams = '?' + 'serviceKey=' + servicekey
-queryParams += '&pagenum=' + pagenum
+queryParams += '&pageNo=' + pagenum
 queryParams += '&numOfRows=' + rows
 queryParams += '&type=' +type
 req = request.Request(url + queryParams)
-req.get_method = lambda: 'GET'
-response_body = request.urlopen(req).read()
-print(response_body)
+response = request.urlopen(req).read().decode('utf-8')
+
+print(response)
