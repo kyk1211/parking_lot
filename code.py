92765b868813331ae8bf5e5ab2df8e13315a89d2
곽윤구 <advsdvs@gmail.com>
2021-06-16 11:04:49 +0900

    '주소출력'

--- code.py
+++ code.py
@@ -1,5 +1,7 @@
 from urllib import request
 import urllib.parse
+import json
+import pandas as pd
 servicekey = 'L%2Bx%2FuV2N4A5xeGN%2FJxtJ3idEtmvq7HMZeioTgu2kROPGZeUz0oSiz5zQiqUj94Oco4%2FcC%2BJNatV8hJf5mL%2BSuQ%3D%3D'
 url = 'http://api.data.go.kr/openapi/tn_pubr_prkplce_info_api'
 pagenum = '0'
@@ -12,4 +14,6 @@ queryParams += '&type=' +type
 req = request.Request(url + queryParams)
 response = request.urlopen(req).read().decode('utf-8')
 
-print(response)
+json_data = json.loads(response)
+for i in range(len(json_data['response']['body']['items'])):
+    print(json_data['response']['body']['items'][i]['lnmadr'])
